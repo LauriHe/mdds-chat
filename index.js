08621a9b5015b055b3ef2db1bb18df8f0c1d1aab
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2023-09-11 17:36:16 +0300

    indexjs

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
+const http = require('http').createServer(app);
+io = require('socket.io')(http);
+//const server = http.createServer(app);
+/*const { Server } = require('socket.io');*/
 
-const io = new Server(server, {
+/*const io = new Server(server, {
   cors: {
     origin: 'http://localhost:5173',
   },
-});
+});*/
 
 app.use(express.static('public'));
 
